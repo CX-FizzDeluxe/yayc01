0d66c8caf6fa5b42e0021c3aa865b437bb95074b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-16 22:42:00 +0000

    Thu Nov 16 22:42:00 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -6,60 +6,65 @@ import java.util.*;
 public class FizzBuzz {
 	
     public static String fizzBuzz(Integer number) {
-    	int by3 = isBy3(number);
-    	int by5 = isBy5(number);
-    	int isD = isDeluxe(number);
-    	int sum = by3 + by5 + isD;
-        Map<Integer, String> map = new HashMap<>();
-        map.put(3, "fizz");
-        map.put(5, "buzz");
-        map.put(7, "deluxe");
-        map.put(11, "fake deluxe");
-        map.put(8, "fizz buzz");
-        map.put(10, "fizz deluxe");
-        map.put(14, "fizz fake deluxe");
-        map.put(12, "buzz deluxe");
-        map.put(16, "buzz fake deluxe");
-        map.put(15, "fizz buzz deluxe");
-        map.put(19, "fizz buzz fake deluxe");
-        if (map.containsKey(sum)) {
-        	return map.get(sum);
-        }
-
-		return number + "";
-    }
-
-    private static int isByNum(Integer num, Integer target) {
-    	if (num % target == 0) {
-    		return target;
+    	String res = "";
+    	boolean isNeither = true;
+    	if (isFizz(number)) {
+    		res += "fizz";
+    		isNeither = false;
+    	} 
+    	if (isBuzz(number)) {
+    		res += " buzz";
+    		isNeither = false;
     	}
-    	while (num != 0) {
-    		if (num % 10 == target) {
-    			return target;
+    	if (isDeluxeFizz(number) || isDeluxeBuzz(number)) {
+    		if (number % 2 == 0) {
+    			res += " deluxe";
+    		} else {
+				res += " fake deluxe";
     		}
-    		num /= 10;
     	}
-    	return 0;
+    	if (isNeither)
+			return number + "";
+		if (res.startsWith(" ")){
+			res = res.substring(1);
+		}
+		return res;
     }
 
-    private static int isDeluxe(Integer num) {
-    	boolean isEven = (num % 2 == 0);
-    	if (num < 10) return 0;
-    	int n = num % 10;
-    	while (num != 0) {
-    		if (num % 10 != n) return 0;
-    		num /= 10;
+    private static boolean isByNum(Integer num, Integer target) {
+    	if (num % target == 0) {
+    		return true;
     	}
-    	return isEven ? 7 : 11;
+    	return isDeluxe(num, target);
     }
 
-    private static int isBy5(Integer num) {
+    private static boolean isDeluxe(Integer num, int target) {
+    	//contains target in num
+    	int temp = num;
+    	while (temp != 0) {	
+    		if (temp % 10 == target) {
+    			return true;
+    		}
+    		temp /= 10;
+    	}
+    	return false;
+    }
+
+    private static boolean isBuzz(Integer num) {
     	return isByNum(num, 5);
     }
 
-	private static int isBy3(Integer num) {
+	private static boolean isFizz(Integer num) {
     	return isByNum(num, 3);
     }
 
+    private static boolean isDeluxeFizz(Integer num) {
+    	if (!isFizz(num)) return false;
+    	return isDeluxe(num, 3);
+    }
 
+    private static boolean isDeluxeBuzz(Integer num) {
+    	if (!isBuzz(num)) return false;
+    	return isDeluxe(num, 5);
+    }
 }
