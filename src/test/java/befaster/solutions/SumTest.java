1ba245ba661440224fbf3fe12a5e35a971dd76cb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-16 22:39:00 +0000

    Thu Nov 16 22:39:00 GMT 2017

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -10,24 +10,11 @@ public class SumTest {
     @Test
     public void compute_sum() {
         assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
-        assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
-        assertThat(FizzBuzz.fizzBuzz(5131), equalTo("fizz buzz"));
-        assertThat(FizzBuzz.fizzBuzz(31), equalTo("fizz"));
         assertThat(FizzBuzz.fizzBuzz(13), equalTo("fizz"));
         assertThat(FizzBuzz.fizzBuzz(12), equalTo("fizz"));
-        assertThat(FizzBuzz.fizzBuzz(5), equalTo("buzz"));
-        assertThat(FizzBuzz.fizzBuzz(10), equalTo("buzz"));
-        assertThat(FizzBuzz.fizzBuzz(511), equalTo("buzz"));
-        assertThat(FizzBuzz.fizzBuzz(555), equalTo("fizz buzz fake deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz fake deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(22), equalTo("deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(11), equalTo("fake deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(1111), equalTo("fake deluxe"));
-
-        assertThat(FizzBuzz.fizzBuzz(33), equalTo("fizz fake deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(222), equalTo("fizz deluxe"));
-
-
+		assertThat(FizzBuzz.fizzBuzz(123), equalTo("fizz fake deluxe"));
+		assertThat(FizzBuzz.fizzBuzz(345), equalTo("fizz buzz fake deluxe"));
+		assertThat(FizzBuzz.fizzBuzz(354), equalTo("fizz buzz deluxe"));
         assertThat(Sum.sum(0, 0), equalTo(0));
     }
 }
