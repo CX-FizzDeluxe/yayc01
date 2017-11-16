ebd48d269cdd14bdbc8aef2d9911cacc8b5076ff
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-16 21:01:54 +0000

    Thu Nov 16 21:01:54 GMT 2017

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -9,6 +9,10 @@ public class SumTest {
 
     @Test
     public void compute_sum() {
-        assertThat(Sum.sum(1, 1), equalTo(2));
+        assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
+        assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+        assertThat(FizzBuzz.fizzBuzz(3), equalTo("fizz"));
+        assertThat(FizzBuzz.fizzBuzz(5), equalTo("buzz"));
+        assertThat(Sum.sum(0, 0), equalTo(0));
     }
 }
