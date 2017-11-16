143d904a9a8ca28a6d63a5694deeb9d5689d720c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-16 21:59:25 +0000

    Thu Nov 16 21:59:25 GMT 2017

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -19,7 +19,7 @@ public class SumTest {
         assertThat(FizzBuzz.fizzBuzz(10), equalTo("buzz"));
         assertThat(FizzBuzz.fizzBuzz(511), equalTo("buzz"));
         assertThat(FizzBuzz.fizzBuzz(555), equalTo("fizz buzz fake deluxe"));
-        assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz deluxe"));
+        assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz fake deluxe"));
         assertThat(FizzBuzz.fizzBuzz(22), equalTo("deluxe"));
         assertThat(FizzBuzz.fizzBuzz(11), equalTo("fake deluxe"));
         assertThat(FizzBuzz.fizzBuzz(1111), equalTo("fake deluxe"));
